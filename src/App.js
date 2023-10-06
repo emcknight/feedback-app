fc07c30f9bab78b838627b432dc7a9cc81d77d6f
Eric McKnight <emcknig8@students.kennesaw.edu>
2023-10-06 17:39:36 -0400

    Context setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import { useState } from "react";
-import { v4 as uuidv4 } from "uuid";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { FeedbackProvider } from "./context/FeedbackContext";
 import Header from "./components/Header";
-import FeedbackData from "./data/FeedbackData";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
@@ -10,41 +8,29 @@ import AboutPage from "./components/pages/AboutPage";
 import AboutIconLink from "./components/AboutIconLink";
 
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData);
-  const deleteFeedback = (id) => {
-    if (window.confirm("Are you sure you want to delete?")) {
-      setFeedback(feedback.filter((item) => item.id !== id));
-    }
-  };
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
-  };
-
   return (
-    <Router>
-      <Header />
-      <div className="container">
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <FeedbackForm handleAdd={addFeedback} />
-                <FeedbackStats feedback={feedback} />
-                <FeedbackList
-                  feedback={feedback}
-                  handleDelete={deleteFeedback}
-                />
-              </>
-            }
-          />
-          <Route path="/about" element={<AboutPage />} />
-        </Routes>
+    <FeedbackProvider>
+      <Router>
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <FeedbackForm />
+                  <FeedbackStats />
+                  <FeedbackList />
+                </>
+              }
+            />
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
 
-        <AboutIconLink />
-      </div>
-    </Router>
+          <AboutIconLink />
+        </div>
+      </Router>
+    </FeedbackProvider>
   );
 }
 
--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,56 +1,66 @@
-import {useState} from 'react'
-import Card from "./shared/Card"
-import Button from './shared/Button'
-import RatingSelect from './RatingSelect'
+import { useState } from "react";
+import { useContext } from "react";
+import Card from "./shared/Card";
+import Button from "./shared/Button";
+import RatingSelect from "./RatingSelect";
+import FeedbackContext from "../context/FeedbackContext";
 
-function FeedbackForm({handleAdd}) {
-  const [text, setText] = useState('')
-  const [rating, setRating] = useState(10)
-  const [btnDisabled, setBtnDisabled] = useState(true)
-  const [message, setMessage] = useState('')
+function FeedbackForm() {
+  const [text, setText] = useState("");
+  const [rating, setRating] = useState(10);
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
+  
+  const { addFeedback } = useContext(FeedbackContext);
 
-
-  const handleTextChange = ({target: {value} }) => {
-    if (value === '') {
-      setBtnDisabled(true)
-      setMessage(null)
-    } else if(value !== '' && value.trim().length < 10){
-      setBtnDisabled(true)
-      setMessage('Text must be at least 10 characters.')
+  const handleTextChange = ({ target: { value } }) => {
+    if (value === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (value !== "" && value.trim().length < 10) {
+      setBtnDisabled(true);
+      setMessage("Text must be at least 10 characters.");
     } else {
-      setBtnDisabled(false)
-      setMessage(null)
+      setBtnDisabled(false);
+      setMessage(null);
     }
-    setText(value)
-  }
+    setText(value);
+  };
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    if(text.trim().length >= 10){
+    e.preventDefault();
+    if (text.trim().length >= 10) {
       const newFeedback = {
         text: text,
         rating: rating,
-      }
+      };
 
-      handleAdd(newFeedback)
+      addFeedback(newFeedback);
 
-      setText('')
+      setText("");
     }
-  }
+  };
 
   return (
     <Card>
       <form onSubmit={handleSubmit}>
         <h2>How would you rate your service with us?</h2>
-        <RatingSelect select={(rating) => setRating(rating)}/>
+        <RatingSelect select={(rating) => setRating(rating)} />
         <div className="input-group">
-          <input onChange={handleTextChange} type="text" placeholder="Write a review" value={text}/>
-          <Button type="submit" isDisabled={btnDisabled}>Send</Button>
+          <input
+            onChange={handleTextChange}
+            type="text"
+            placeholder="Write a review"
+            value={text}
+          />
+          <Button type="submit" isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
         {message && <div className="message">{message}</div>}
       </form>
     </Card>
-  )
+  );
 }
 
-export default FeedbackForm
+export default FeedbackForm;
